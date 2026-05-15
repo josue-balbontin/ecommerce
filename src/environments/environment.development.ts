ba9507da1a0a89b9d46c74aed14a6825a727feaf
josue-balbontin <balbontinjosue@gmail.com>
2026-05-15 09:14:53 -0400

    subiendo env

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    SUPABASE_URL: 'https://mfhhaqgxrlnozbplsmec.supabase.co',
+    SUPABASE_KEY: 'sb_publishable_nbB27SbPWulNwxn-tm710Q_LAfjTteE'
+};
